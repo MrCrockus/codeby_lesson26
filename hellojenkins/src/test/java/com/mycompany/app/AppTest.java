4dd977d0f14e35ef8ac73696453b4e9c33ce4bd3
Som <block159@mail.ru>
2025-09-13 12:34:08 +0700

    Update AppTest.java

--- hellojenkins/src/test/java/com/mycompany/app/AppTest.java
+++ hellojenkins/src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello Jenkinser!", app.getMessage());
+        assertEquals("Hello Jenkins!", app.getMessage());
     }
 }
